3ae8379c0036d276cc3656e93032fb3d6c62b7f6
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-12 11:28:21 -0400

    Update Desktop.js
    multi windows

--- Desktop.js
+++ Desktop.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
+import backgroundImage from './osBG.jpg'; // change the path to your actual image
 import Taskbar from './Taskbar';
 import StartMenu from './StartMenu';
 import Window from './Window';
@@ -9,14 +10,17 @@ const StyledDesktop = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
-  background-color: #a8a8a8;
+  background-image: url(${backgroundImage});
+  background-size: cover; // this will ensure your image covers the entire element
+  background-position: center; // this centers the image
   overflow: hidden;
 `;
 
+
 function Desktop() {
   const [apps, setApps] = useState([
-    { name: 'App 1', open: () => openApp('App 1', <div>App 1</div>) },
-    { name: 'App 2', open: () => openApp('App 2', <div>App 2</div>) },
+    { name: 'App 1', component: <div>App 1</div> },
+    { name: 'App 2', component: <div>App 2</div> },
   ]);
 
   const [openApps, setOpenApps] = useState([]);
@@ -31,21 +35,25 @@ function Desktop() {
     setOpenApps([...openApps, { id: uuidv4(), name, component, state: 'normal', zIndex: currentZIndex + 1 }]);
   };
 
-  const minimizeApp = (index) => {
-    setOpenApps(openApps.map((app, i) => i === index ? { ...app, state: 'minimized' } : app));
+  const minimizeApp = (id) => {
+    setOpenApps(openApps.map((app) => app.id === id ? { ...app, state: 'minimized' } : app));
   };
 
-  const maximizeApp = (index) => {
-    setOpenApps(openApps.map((app, i) => i === index ? { ...app, state: app.state === 'maximized' ? 'normal' : 'maximized' } : app));
+  const maximizeApp = (id) => {
+    setOpenApps(openApps.map((app) => app.id === id ? { ...app, state: app.state === 'maximized' ? 'normal' : 'maximized' } : app));
   };
 
-  const closeApp = (index) => {
-    setOpenApps(openApps.filter((app, i) => i !== index));
+  const closeApp = (id) => {
+    setOpenApps(openApps.filter((app) => app.id !== id));
   };
 
   const bringToFront = (id) => {
     setCurrentZIndex(currentZIndex + 1);
     setOpenApps(openApps.map((app) => app.id === id ? { ...app, zIndex: currentZIndex } : app));
+  };
+
+  const restoreApp = (id) => {
+    setOpenApps(openApps.map((app) => app.id === id ? { ...app, state: 'normal' } : app));
   };
 
   return (
@@ -56,15 +64,16 @@ function Desktop() {
           title={app.name}
           zIndex={app.zIndex}
           onClick={() => bringToFront(app.id)}
-          minimize={() => minimizeApp(index)}
-          maximize={() => maximizeApp(index)}
-          close={() => closeApp(index)}
+          minimize={() => minimizeApp(app.id)}
+          maximize={() => maximizeApp(app.id)}
+          close={() => closeApp(app.id)}
+          state={app.state}
         >
           {app.component}
         </Window>
       ))}
-      <Taskbar toggleStartMenu={toggleStartMenu} openApps={openApps} />
-      {isStartMenuOpen && <StartMenu apps={apps} />}
+      <Taskbar toggleStartMenu={toggleStartMenu} openApps={openApps} restoreApp={restoreApp} />
+      {isStartMenuOpen && <StartMenu apps={apps.map(app => ({ ...app, open: () => openApp(app.name, app.component) }))} />}
     </StyledDesktop>
   );
 }
