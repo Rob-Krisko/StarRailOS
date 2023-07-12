7bdbe2db1978e6db52a13ebf70aee8baf5e64b77
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-12 10:58:26 -0400

    Update App.js
    version 2.0

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import TaskBar from './TaskBar';
-import WindowManager from './WindowManager';
-import store from './store';
 import './App.css';
+import Desktop from './components/Desktop';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <WindowManager />
-        <TaskBar />
-      </div>
-    </Provider>
+    <div className="App">
+      <Desktop />
+    </div>
   );
 }
 
