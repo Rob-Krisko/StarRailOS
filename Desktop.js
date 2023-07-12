593c6b05791fded2b8f20cb48740c1aba00af8df
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-12 15:24:09 -0400

    Update Desktop.js
    calculator and weather widget

--- Desktop.js
+++ Desktop.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import backgroundImage from './osBG.jpg';
 import { v4 as uuidv4 } from 'uuid';
-import backgroundImage from './osBG.jpg'; // change the path to your actual image
 import Taskbar from './Taskbar';
 import StartMenu from './StartMenu';
 import Window from './Window';
+import Calculator from './Calculator';
+import WeatherWidget from './WeatherWidget';
 
 const StyledDesktop = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
   background-image: url(${backgroundImage});
-  background-size: cover; // this will ensure your image covers the entire element
-  background-position: center; // this centers the image
+  background-size: cover;
+  background-position: center;
   overflow: hidden;
 `;
 
-
 function Desktop() {
   const [apps, setApps] = useState([
+    { name: 'Calculator', component: <Calculator /> },
     { name: 'App 1', component: <div>App 1</div> },
     { name: 'App 2', component: <div>App 2</div> },
   ]);
@@ -33,6 +35,7 @@ function Desktop() {
 
   const openApp = (name, component) => {
     setOpenApps([...openApps, { id: uuidv4(), name, component, state: 'normal', zIndex: currentZIndex + 1 }]);
+    setCurrentZIndex(currentZIndex + 1);
   };
 
   const minimizeApp = (id) => {
@@ -49,15 +52,17 @@ function Desktop() {
 
   const bringToFront = (id) => {
     setCurrentZIndex(currentZIndex + 1);
-    setOpenApps(openApps.map((app) => app.id === id ? { ...app, zIndex: currentZIndex } : app));
+    setOpenApps(openApps.map((app) => app.id === id ? { ...app, zIndex: currentZIndex + 1 } : app));
   };
 
   const restoreApp = (id) => {
     setOpenApps(openApps.map((app) => app.id === id ? { ...app, state: 'normal' } : app));
+    setCurrentZIndex(currentZIndex + 1);
   };
 
   return (
     <StyledDesktop>
+      <WeatherWidget />
       {openApps.map((app, index) => (
         <Window 
           key={app.id}
