de4a64de5054649ab0f7ec9af1d231bb1151d23c
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-15 15:58:56 -0400

    Update App.js
    Login and logout

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
-import Desktop from './Desktop';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Desktop from './components/Desktop';
 
 function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
+
+  const handleLogin = (username, password) => {
+    const user = { username, password };
+    localStorage.setItem('user', JSON.stringify(user));
+    setUser(user);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
   return (
-    <div className="App">
-      <Desktop />
-    </div>
+    user 
+      ? <Desktop onLogout={handleLogout} /> 
+      : <Login onLogin={handleLogin} />
   );
 }
 
