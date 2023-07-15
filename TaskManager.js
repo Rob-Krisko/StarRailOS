27957d74c1f45fc90668ab7d6ac856981ec2fc07
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-14 20:27:10 -0400

    Create TaskManager.js

---
+++ TaskManager.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { TaskbarContext } from './TaskbarContext';
+
+const TaskManagerContainer = styled.div`
+  padding: 20px;
+`;
+
+const Task = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  padding: 10px;
+`;
+
+const CloseButton = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+function TaskManager() {
+  const { openApps, closeApp } = useContext(TaskbarContext);
+
+  return (
+    <TaskManagerContainer>
+      {openApps.map(app => (
+        <Task key={app.id}>
+          <span>{app.name}</span>
+          <CloseButton onClick={(event) => {
+            event.stopPropagation();
+            closeApp(app.id);
+          }}>X</CloseButton>
+        </Task>
+      ))}
+    </TaskManagerContainer>
+  );
+}
+
+export default TaskManager;
