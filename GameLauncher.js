72d3f7fc84952c9b548bea3878252607d792452e
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-16 13:55:39 -0400

    Update GameLauncher.js

--- GameLauncher.js
+++ GameLauncher.js
@@ -1,52 +1,69 @@
 import React, { useContext } from 'react';
-import { TaskbarContext } from './TaskbarContext';
-import CounterGame from './CounterGame';
 import styled from 'styled-components';
+import { TaskbarContext } from './TaskbarContext';
+import TicTacToe from './TicTacToe';
+import CounterGame from './CounterGame';
+import bronyaIcon from '../assets/bronya_icon.png';
 import silverIcon from '../assets/silver_icon.png';
 
-const GameLauncher = () => {
-  const { openApp } = useContext(TaskbarContext);
-  const games = [{ name: 'Counter Game', icon: silverIcon }]; // Add your game names here with icons
+const GameLauncherContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px;
+  justify-content: space-around;
+`;
 
-  const GameButton = styled.button`
-    background-color: transparent;
-    border: none;
-    text-align: center;
-    margin: 10px;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-  `;
+const GameCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  color: #fff; // Make the text white
 
-  const GameIcon = styled.img`
-    width: 100px;
-    height: 100px;
-  `;
-
-  // update the switch statement with games
-  const launchGame = (gameName) => {
-    let gameComponent;
-    switch(gameName) {
-      case 'Counter Game':
-        gameComponent = <CounterGame />;
-        break;
-      default:
-        gameComponent = <div>Game not found.</div>;
-    }
-    openApp(gameName, gameComponent);
+  &:hover {
+    transform: scale(1.05);
   }
+`;
+
+
+const GameIcon = styled.img`
+  width: 64px;
+  height: 64px;
+`;
+
+function GameLauncher() {
+  const { openApp } = useContext(TaskbarContext);
+
+  const games = [
+    {
+      name: "Tic Tac Toe",
+      icon: bronyaIcon,
+      component: <TicTacToe />
+    },
+    {
+      name: "Counter Game",
+      icon: silverIcon,
+      component: <CounterGame />
+    },
+    // ...
+    // add more games in the same fashion as above. make sure to save icon in the assets directory and import at the top as well
+    // ...
+  ];
 
   return (
-    <div style={{backgroundColor: "rgba(0, 0, 0, 0.7)", padding: "10px", borderRadius: "5px", color: "white"}}>
-      <h1>Game Launcher</h1>
-      {games.map(game => 
-        <GameButton key={game.name} onClick={() => launchGame(game.name)}>
+    <GameLauncherContainer>
+      {games.map((game, index) => (
+        <GameCard key={index} onClick={() => openApp(game.name, game.component)}>
           <GameIcon src={game.icon} alt={game.name} />
-          <br/>
-          {game.name}
-        </GameButton>
-      )}
-    </div>
+          <p>{game.name}</p>
+        </GameCard>
+      ))}
+    </GameLauncherContainer>
   );
 }
 
