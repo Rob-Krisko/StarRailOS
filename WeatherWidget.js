01344afc3c59305630ad3f6351011f6dd2810f65
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-20 15:11:56 -0400

    http to https and restyled

--- WeatherWidget.js
+++ WeatherWidget.js
@@ -5,9 +5,11 @@ const WidgetContainer = styled.div`
   position: absolute;
   top: 10px;
   right: 10px;
-  background-color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(0, 0, 0, 0.8);
+  color: #ddd;
   padding: 10px;
   border-radius: 5px;
+  box-shadow: 0px 0px 15px 5px rgba(0,255,255,.75);
 `;
 
 const WeatherForm = styled.form`
@@ -16,17 +18,32 @@ const WeatherForm = styled.form`
 
 const WeatherTitle = styled.h2`
   margin: 0;
+  color: #0ff;
 `;
 
 const WeatherTemp = styled.p`
   font-size: 1.5em;
   margin: 0;
+  color: #0ff;
 `;
 
 const WeatherCondition = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+`;
+
+const Button = styled.button`
+  color: #0ff;
+  background-color: rgba(0,0,0,0.4);
+  border: 1px solid #0ff;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-top: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(0,255,255,0.2);
+  }
 `;
 
 function WeatherWidget() {
@@ -42,7 +59,7 @@ function WeatherWidget() {
   const fetchWeather = (city) => {
     setIsLoading(true);
     setError(null);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=db65b051b1ce560a620362330f5e0542`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=db65b051b1ce560a620362330f5e0542`)
       .then(response => response.json())
       .then(data => {
         if (data.cod === '404') {
@@ -78,7 +95,7 @@ function WeatherWidget() {
             {weatherData.weather.map((condition) => (
               <WeatherCondition key={condition.id}>
                 <img
-                  src={`http://openweathermap.org/img/w/${condition.icon}.png`}
+                  src={`https://openweathermap.org/img/w/${condition.icon}.png`}
                   alt={condition.description}
                 />
                 <span>{condition.description}</span>
@@ -86,7 +103,7 @@ function WeatherWidget() {
             ))}
             <div>Humidity: {weatherData.main.humidity}%</div>
             <div>Wind: {weatherData.wind.speed} mph</div>
-            <button onClick={updateCity}>Change city</button>
+            <Button onClick={updateCity}>Change city</Button>
           </>
         )
       )}
