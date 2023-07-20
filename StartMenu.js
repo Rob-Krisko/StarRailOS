ea19226d038b62e0d7b83b589c98c1caa5e58d7c
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-20 15:39:08 -0400

    re styled

--- StartMenu.js
+++ StartMenu.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import dummyIcon from '../assets/dummy.png';
 
 const StyledMenu = styled.div`
   position: absolute;
   bottom: 30px;
   width: 200px;
-  background-color: #fff;
-  border: 1px solid #000;
+  background-color: rgba(0, 0, 0, 0.8);
+  border: 1px solid #0ff;
   z-index: 9999;
+  color: #0ff;
 `;
 
 const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
   padding: 10px;
-  border-bottom: 1px solid #000;
+  border-bottom: 1px solid #0ff;
   cursor: pointer;
 
   &:hover {
-    background-color: #eee;
+    background-color: rgba(0, 255, 255, 0.1);
   }
 `;
 
 const LogoutButton = styled.button`
-  // style the logout button as you need
+  padding: 10px;
+  background: none;
+  border: none;
+  color: #0ff;
+  cursor: pointer;
+  width: 100%;
+
+  &:hover {
+    background-color: rgba(0, 255, 255, 0.1);
+  }
 `;
 
 function StartMenu({ apps, onLogout, toggleStartMenu }) {
@@ -34,6 +48,7 @@ function StartMenu({ apps, onLogout, toggleStartMenu }) {
     <StyledMenu>
       {apps.map((app, index) => (
         <MenuItem key={index} onClick={() => handleClick(app)}>
+          <img src={dummyIcon} alt="app-icon" width={20} height={20} />
           {app.name}
         </MenuItem>
       ))}
