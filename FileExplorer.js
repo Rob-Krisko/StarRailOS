4badfef75fd799abfecbbfde0e9090ff11c514f6
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-07-23 15:58:47 -0400

    Update FileExplorer.js

--- FileExplorer.js
+++ FileExplorer.js
@@ -2,27 +2,64 @@ import React, { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import { TaskbarContext } from './TaskbarContext';
 import TextEditor from './TextEditor';
+import fileIcon from '../assets/text-icon.png'; // Imported file icon
+import plusIcon from '../assets/plus-icon.png'; // Import your plus icon here
+import deleteIcon from '../assets/delete-icon.png'; // Import your delete icon here
 
 const StyledExplorerContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  justify-content: flex-start;
-  box-sizing: border-box;
-  overflow: auto; 
-  flex-grow: 1;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  padding: 20px;
+  justify-items: center;
 `;
 
-
-const File = styled.div`
+const FileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #000; // Add a border to make the container more distinct
   padding: 10px;
   background-color: #fff;
-  margin: 5px 0;
   cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
   &:hover {
     background-color: #f0f0f0;
   }
+`;
+
+const FileIcon = styled.img`
+  width: 50px; // Adjust as needed
+  height: 50px; // Adjust as needed
+`;
+
+const FileName = styled.span`
+  margin-top: 10px;
+  text-align: center;
+  word-break: break-word;
+`;
+
+const NewFileButton = styled.button`
+  background: url(${plusIcon}) no-repeat center center; // Plus icon for the button
+  background-size: contain;
+  height: 50px; // Adjust as needed
+  border: none;
+  cursor: pointer;
+  display: flex; 
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
+const DeleteFileButton = styled.button`
+  background: url(${deleteIcon}) no-repeat center center; // Delete icon for the button
+  background-size: contain;
+  width: 20px; // Adjust as needed
+  height: 20px; // Adjust as needed
+  border: none;
+  cursor: pointer;
+  margin-top: 5px;
 `;
 
 function FileExplorer() {
@@ -56,11 +93,28 @@ function FileExplorer() {
     openApp("Text Editor", <TextEditor initialFile={{ fileName, fileContent }} />);
   };
 
+  const deleteFile = (fileName) => {
+    const confirmation = window.confirm("Are you sure you want to delete this file?");
+    if (confirmation) {
+      localStorage.removeItem(fileName);
+      setFileSystem(fileSystem.filter(file => file.name !== fileName));
+    }
+  };
+
+  const createNewFile = () => {
+    openApp("Text Editor", <TextEditor initialFile={null} />);
+  };
+
   return (
     <StyledExplorerContainer>
-      {fileSystem.map((file) => 
-        <File key={file.name} onClick={() => openFile(file.name, file.content)}>{file.name}</File>
-      )}
+      {fileSystem.map((file) => (
+        <FileWrapper key={file.name}>
+          <FileIcon src={fileIcon} alt="file icon" onClick={() => openFile(file.name, file.content)} />
+          <FileName>{file.name}</FileName>
+          <DeleteFileButton onClick={() => deleteFile(file.name)} />
+        </FileWrapper>
+      ))}
+      <NewFileButton onClick={createNewFile} />
     </StyledExplorerContainer>
   );
 }
